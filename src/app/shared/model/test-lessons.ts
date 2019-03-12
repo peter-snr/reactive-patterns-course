1c9d50c7bcfc4bd2d998b04393cbf1c59ac50b68
peter-snr <pgoudman@gmail.com>
2019-03-12 14:38:03 +0000

    Event Bus Functioning to notify, no add yet

---
+++ src/app/shared/model/test-lessons.ts
@@ -0,0 +1,20 @@
+import {Lesson} from './lesson';
+
+export const testLessons: Lesson[] = [
+  {
+    id: 1,
+    description: 'Setting Up an Angular Development Environment',
+    duration: '5:00'
+  },
+    {
+    id: 2,
+    description: 'Running the Lessons Code',
+    duration: '7:00'
+  },
+    {
+    id: 1,
+    description: 'Building Your First App - Hello World Step By Step',
+    duration: '9:00'
+  },
+];
+
